2d33b01b4943fba492f7e93943d4c7cf7831a184
RobW267 <66453164+RobW267@users.noreply.github.com>
2020-06-04 21:40:24 -0500

    Update geometry.cpp

--- source/geometry.cpp
+++ source/geometry.cpp
@@ -1,6 +1,10 @@
 #include "geometry.h"
 
 
+GeometryE::GeometryE() {
+	map = nullptr;
+}
+
 void Geometry::addCon(unsigned char c1, unsigned char c2) {
 	geometry[c1].neighbors.push_back(&geometry[c2]);
 	geometry[c2].neighbors.push_back(&geometry[c1]);
