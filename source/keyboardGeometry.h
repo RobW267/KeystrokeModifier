1a004d595d97c1fadb8ebefd11e90a1fc8886ea3
RobW267 <66453164+RobW267@users.noreply.github.com>
2020-06-04 21:44:19 -0500

    Create keyboardGeometry.h

---
+++ source/keyboardGeometry.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "geometry.h"
+double dRand();
+struct keyboardGeometry;
+{
+public:
+  Geometry keyboard;
+  keyboardGeometry();
+  ~keyboardGeometry();
+  void beginMap(unsigned char);
+  void endMap(unsigned char);
+  unsigned char getMap(unsigned char);
+};
